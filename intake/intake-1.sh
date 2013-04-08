d8bd37cea9baaa39bce670e0212b0f8075d8be86
Eric Kow <eric.kow@gmail.com>
2013-04-08 06:44:00 +0000

    Add my segmenter to the intake script.
    Might as well use it as a preprocessor before manual segmentation.
    
    git-svn-id: https://wwwsecu.irit.fr/svn/Stac@1066 2e4a9ccd-9a2a-43a5-8ce1-c04977dfc52d

--- intake/intake-1.sh
+++ intake/intake-1.sh
@@ -20,6 +20,9 @@ OUTPUT_BNAME=$2
 pushd $SCRIPT_DIR/../txt2csv > /dev/null
 TXT2CSV_DIR=$PWD
 popd > /dev/null
+pushd $SCRIPT_DIR/../segmentation > /dev/null
+SEGMENTATION_DIR=$PWD
+popd > /dev/null
 
 mkdir -p unsegmented
 mkdir -p segmented
@@ -28,7 +31,10 @@ TURNS_FILE=$OUTPUT_BNAME.turns
 python $TXT2CSV_DIR/extract_turns.py $INPUT_FILE > $TURNS_FILE
 python $TXT2CSV_DIR/extract_annot.py $TURNS_FILE
 cp ${TURNS_FILE}csv unsegmented/${OUTPUT_BNAME}.soclog.csv
-cp ${TURNS_FILE}csv segmented/${OUTPUT_BNAME}.soclog.seg.csv
+# presegment automatically (human would still have to clean up)
+python $SEGMENTATION_DIR/simple-segments  --csv\
+      unsegmented/${OUTPUT_BNAME}.soclog.csv\
+      segmented/${OUTPUT_BNAME}.soclog.seg.csv
 rm $TURNS_FILE ${TURNS_FILE}csv
 
 echo >&2 "Now edit segmented/$OUTPUT_BNAME.soclog.seg.csv (in eg. Excel)"
