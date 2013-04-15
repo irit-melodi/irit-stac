37495e0dce4351ff11603008a35e6508160be308
Eric Kow <eric.kow@gmail.com>
2013-04-15 08:08:24 +0000

    Generalise rename-series to match different series
    Using --input-suffix and --output-prefix
    (--input-prefix captured by pattern
     --output-suffix defaults to --input-suffix)
    lets you match on arbitrary patterns
    
    git-svn-id: https://wwwsecu.irit.fr/svn/Stac@1092 2e4a9ccd-9a2a-43a5-8ce1-c04977dfc52d

--- intake/rename-series.py
+++ intake/rename-series.py
@@ -23,6 +23,7 @@ Input should be a template, so for the above example:
 
 """
 
+import itertools
 import glob
 import math
 import os
@@ -38,6 +39,19 @@ from itertools import chain
 arg_parser = argparse.ArgumentParser(description='Rename sequence of files.')
 arg_parser.add_argument('pattern', metavar='FILENAME'
                        , help='file name pattern'
+                       , default=''
+                       )
+arg_parser.add_argument('--input-suffix', metavar='STRING'
+                       , action='store'
+                       , help='[advanced] (pattern interpreted as a prefix)'
+                       )
+arg_parser.add_argument('--output-prefix', metavar='STRING'
+                       , action='store'
+                       , help='[advanced]'
+                       )
+arg_parser.add_argument('--output-suffix', metavar='STRING'
+                       , action='store'
+                       , help='[advanced] (default: input-suffix)'
                        )
 arg_parser.add_argument('--verbose', '-v', action='store_true')
 arg_parser.add_argument('--dry-run', '-n', action='store_true')
@@ -47,22 +61,47 @@ args=arg_parser.parse_args()
 # main
 # ---------------------------------------------------------------------
 
-(pattern_bname, pattern_ext) = os.path.splitext(args.pattern)
+advanced_args = [ args.input_suffix
+                , args.output_prefix
+                ]
+
+
+if any(advanced_args + [args.output_suffix]):
+    if not all(advanced_args):
+        arg_parser.print_help()
+        print >> sys.stderr, "ERROR: All advanced args are required for advanced mode"
+        sys.exit(1)
+    else:
+        if not args.output_suffix: args.output_suffix = args.input_suffix
+        args.input_prefix = args.pattern
+        advanced_mode = True
+else:
+    advanced_mode = False
+
+if advanced_mode:
+    (pattern_bname, pattern_ext) = (args.input_prefix, args.input_suffix)
+else:
+    (pattern_bname, pattern_ext) = os.path.splitext(args.pattern)
+
 pattern_glob   = pattern_bname + '[0-9]*' + pattern_ext
 pattern_pieces = pattern_bname.split(os.extsep)
 
-output_prefix = pattern_pieces[0]
-output_ext    = os.extsep.join(pattern_pieces[1:]) + pattern_ext
+if advanced_mode:
+    output_prefix = args.output_prefix
+    output_ext    = args.output_suffix
+else:
+    output_prefix = pattern_pieces[0]
+    output_ext    = os.extsep.join([''] + pattern_pieces[1:]) + pattern_ext
 
 files = list(glob.glob(pattern_glob))
 size   = len(files)
 if size > 0:
-    digits = int(math.ceil(math.log10(size)))
+    digits = max(2,int(math.ceil(math.log10(size))))
 else:
     digits = 0
 
 if args.verbose:
-    output_template = '%s_%s.%s' % (output_prefix, 'X' * digits, output_ext)
+    output_template = '%s_%s%s' % (output_prefix, 'X' * digits, output_ext)
     msgs =\
         [ 'Pattern   %s [%d matches]' % (pattern_glob, size)
         , 'Saving to %s [%d digits]'  % (output_template, digits)
@@ -72,7 +111,8 @@ if args.verbose:
 def new_name(f):
     strip_len = len(pattern_bname)
     suffix    = os.path.splitext(f)[0][strip_len:]
-    return '%s_%s.%s' % (output_prefix, suffix.zfill(digits), output_ext)
+    suffix    = "".join(itertools.takewhile(lambda x:x.isdigit(), suffix))
+    return '%s_%s%s' % (output_prefix, suffix.zfill(digits), output_ext)
 
 for f in files:
     f2 = new_name(f)
