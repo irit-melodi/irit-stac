ceb6eea051f4ed8477f02530801c1ce6654e3640
Eric Kow <eric.kow@gmail.com>
2013-04-15 08:08:41 +0000

    More robustness in text2csv/split_csv.py
    - Deal with 'awkward' lines where the entire CSV row was quoted
      for some bizarre reason
    - Avoid stripping trailing whitespace (end of line markers only)
      as these files are tab-delimited
    
    git-svn-id: https://wwwsecu.irit.fr/svn/Stac@1099 2e4a9ccd-9a2a-43a5-8ce1-c04977dfc52d

--- txt2csv/split_csv.py
+++ txt2csv/split_csv.py
@@ -14,7 +14,7 @@ output toto1.csv, toto2.csv, etc (in same dir as original file)
 
 import sys
 import os.path
-
+import csv  # only for use with quoted lines corner case
 
 infile = open(sys.argv[1]).readlines()
 base, ext = os.path.splitext(sys.argv[1])
@@ -24,15 +24,40 @@ header = infile[0].strip()
 nb = len(infile)
 current = [header]
 
+def awkward(line):
+    """
+    There is the odd case where an entire row appears to have been
+    quoted; what's tricky about these is that text with quotation
+    marks in its will be escaped.
+    luckily, csvreader in Python reads any iterator, so we can just give
+    it a singleton list and write it back out
+    """
+    reader  = csv.reader([line]) # should just read as a big single-cell row
+    try:
+        [line2]  = reader.next()
+    except ValueError:
+        print >> sys.stderr, "Bug: was expecting this line to be interpreted as a single column"
+        print >> sys.stderr, line
+        raise
+
+    return line2
+
+def chomped(line):
+    return line.rstrip('\r\n') # only remove newlines; tabs are significant
+
 for (i,line) in enumerate(infile[1:]):
+    line = chomped(line)
+    if len(line) > 1 and line[0] == '"' and line[-1] == '"':
+        line = chomped(awkward(line))
+
     if line.strip()=="" or i==nb-2:
         outf = open("%s%d%s"%(base,count,ext),"w")
         if i==nb-2:
-            current.append(line.strip())
+            current.append(line)
         for one in current:
             print >> outf, one
         outf.close()
         current = [header]
         count = count +1
     else:
-        current.append(line.strip())
+        current.append(line)
