e2bc6662f74c1d8b0d0c81e26c9179228b136e92
Eric Kow <eric.kow@gmail.com>
2013-04-15 08:08:37 +0000

    More flexibility in rename-series
    For use in cleaning up pre-conventional SOCL stuff
    
    git-svn-id: https://wwwsecu.irit.fr/svn/Stac@1097 2e4a9ccd-9a2a-43a5-8ce1-c04977dfc52d

--- intake/rename-series.py
+++ intake/rename-series.py
@@ -37,7 +37,7 @@ from itertools import chain
 # ---------------------------------------------------------------------
 
 arg_parser = argparse.ArgumentParser(description='Rename sequence of files.')
-arg_parser.add_argument('pattern', metavar='FILENAME'
+arg_parser.add_argument('input_prefix', metavar='FILENAME'
                        , help='file name pattern'
                        , default=''
                        )
@@ -53,6 +53,9 @@ arg_parser.add_argument('--output-suffix', metavar='STRING'
                        , action='store'
                        , help='[advanced] (default: input-suffix)'
                        )
+arg_parser.add_argument('--mv-cmd', metavar='STRING'
+                       , action='store'
+                       , help='command to perform the renaming (eg. "git mv")')
 arg_parser.add_argument('--verbose', '-v', action='store_true')
 arg_parser.add_argument('--dry-run', '-n', action='store_true')
 args=arg_parser.parse_args()
@@ -73,24 +76,25 @@ if any(advanced_args + [args.output_suffix]):
         sys.exit(1)
     else:
         if not args.output_suffix: args.output_suffix = args.input_suffix
-        args.input_prefix = args.pattern
         advanced_mode = True
 else:
     advanced_mode = False
 
+pattern_dir   = os.path.dirname(args.input_prefix)
+pattern_bname = os.path.basename(args.input_prefix)
 if advanced_mode:
-    (pattern_bname, pattern_ext) = (args.input_prefix, args.input_suffix)
+    (pattern_prefix, pattern_ext) = (pattern_bname, args.input_suffix)
 else:
-    (pattern_bname, pattern_ext) = os.path.splitext(args.pattern)
+    (pattern_prefix, pattern_ext) = os.path.splitext(pattern_bname)
 
-pattern_glob   = pattern_bname + '[0-9]*' + pattern_ext
-pattern_pieces = pattern_bname.split(os.extsep)
+pattern_glob   = os.path.join(pattern_dir,pattern_prefix) + '[0-9]*' + pattern_ext
+pattern_pieces = pattern_prefix.split(os.extsep)
 
 if advanced_mode:
     output_prefix = args.output_prefix
     output_ext    = args.output_suffix
 else:
-    output_prefix = pattern_pieces[0]
+    output_prefix = pattern_pieces[0] + "_"
     output_ext    = os.extsep.join([''] + pattern_pieces[1:]) + pattern_ext
 
 files = list(glob.glob(pattern_glob))
@@ -101,7 +105,7 @@ else:
     digits = 0
 
 if args.verbose:
-    output_template = '%s_%s%s' % (output_prefix, 'X' * digits, output_ext)
+    output_template = '%s%s%s' % (output_prefix, 'X' * digits, output_ext)
     msgs =\
         [ 'Pattern   %s [%d matches]' % (pattern_glob, size)
         , 'Saving to %s [%d digits]'  % (output_template, digits)
@@ -109,10 +113,13 @@ if args.verbose:
     print >> sys.stderr, "\n".join(msgs)
 
 def new_name(f):
-    strip_len = len(pattern_bname)
-    suffix    = os.path.splitext(f)[0][strip_len:]
+    strip_len = len(pattern_prefix)
+    dname     = os.path.dirname(f)
+    bname     = os.path.basename(f)
+    suffix    = os.path.splitext(bname)[0][strip_len:]
     suffix    = "".join(itertools.takewhile(lambda x:x.isdigit(), suffix))
-    return '%s_%s%s' % (output_prefix, suffix.zfill(digits), output_ext)
+    return os.path.join(dname, output_prefix)\
+           + suffix.zfill(digits) + output_ext
 
 for f in files:
     f2 = new_name(f)
@@ -120,7 +127,12 @@ for f in files:
         print >> sys.stderr, "%s\t-> %s" % (f,f2)
 
     if not args.dry_run:
-        os.rename(f, f2)
+        if args.mv_cmd:
+            cmd=" ".join([args.mv_cmd, f, f2])
+            if args.verbose: print >> sys.stderr, cmd
+            os.system(cmd)
+        else:
+            os.rename(f, f2)
 
 if args.dry_run:
     print >> sys.stderr, "Dry run, so did not actually do anything"
