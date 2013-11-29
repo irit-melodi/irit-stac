95088af82613617f5af671c47866b2e69562557b
Eric Kow <eric.kow@gmail.com>
2013-11-29 15:44:29 +0000

    normalise-glozz: mode to just adjust paragraph ids
    Used when redoing file portions
    
    git-svn-id: https://wwwsecu.irit.fr/svn/Stac@2089 2e4a9ccd-9a2a-43a5-8ce1-c04977dfc52d

--- intake/normalise-glozz.py
+++ intake/normalise-glozz.py
@@ -23,7 +23,31 @@ def nub(xs):
         if x not in ys: ys.append(x)
     return ys
 
-def tidy(filename, output, zero=False):
+class NormSettings(object):
+    def __init__(self, mode, start):
+        self.mode  = mode
+        self.start = start
+
+default_settings = NormSettings('count', 1)
+
+def mk_new_dates(dates, settings=default_settings):
+    start        = settings.start
+    unique_dates = nub(dates)
+
+    if settings.mode == 'zero':
+        return { str(v):'0' for v in unique_dates    }
+    if settings.mode == 'negonly':
+        # only normalise negative dates (in count mode)
+        pos_dates = filter(lambda x:x >= 0, unique_dates)
+        neg_dates = filter(lambda x:x <  0, unique_dates)
+        pos_dict = { str(v):str(v)  for v in pos_dates }
+        neg_dict = { str(v):str(-i) for i,v in enumerate(neg_dates, start) }
+        return dict(pos_dict.items() + neg_dict.items())
+    else:
+        return { str(v):str(i) for i,v in enumerate(unique_dates, start) }
+
+
+def tidy(filename, output, mode=None):
     tree    = ET.parse(filename)
 
     date_elems = tree.findall('.//creation-date')
@@ -32,10 +56,7 @@ def tidy(filename, output, zero=False):
     dates     = [ int(x.text.strip()) for x in date_elems ]
     unit_ids  = [ x.attrib['id'] for x in unit_elems ]
 
-    if zero:
-        new_dates={ str(v):'0' for v in nub(dates)    }
-    else:
-        new_dates={ str(v):str(i) for i,v in enumerate(nub(dates))    }
+    new_dates = mk_new_dates(dates, mode)
 
     def adjust_id(x):
         xparts = x.rsplit('_', 1)
@@ -67,14 +88,18 @@ arg_parser.add_argument('--output', metavar='DIR'
                        , required=True
                        , help='Output directory'
                        )
-arg_parser.add_argument('--zero', action='store_true'
-                       , help='Set all IDs and dates to 0 (avoid unless you have a good reason)'
+arg_parser.add_argument('--mode', choices=[ 'count', 'zero', 'negonly' ],
+                        help='Normalisation mode'
                        )
+arg_parser.add_argument('--start', type=int, default=1, help='start indices from')
+
 arg_parser.add_argument('--verbose', '-v', action='store_true')
 args=arg_parser.parse_args()
 
 if not os.path.exists(args.output): os.mkdir(args.output)
 
+settings = NormSettings(args.mode, args.start)
+
 for f in args.input:
     f2 = os.path.join(args.output, os.path.basename(f))
-    tidy(f, f2, args.zero)
+    tidy(f, f2, settings)
