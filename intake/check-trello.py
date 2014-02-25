4f946f88f72b54c960a4b24ed62b74162624b1b3
Eric Kow <eric.kow@gmail.com>
2014-02-25 11:14:32 +0000

    Small helper script to check anno status on trello
    git-svn-id: https://wwwsecu.irit.fr/svn/Stac@2398 2e4a9ccd-9a2a-43a5-8ce1-c04977dfc52d

---
+++ intake/check-trello.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Author: Eric Kow
+# License: BSD3
+
+"""
+Populate the Trello STAC annotation workflow API
+"""
+
+import collections
+
+import trello as tr
+import secrets  # api-key, etc, not for revision control
+
+BOARD_ID = '52453e3d9f6437111f001775'
+
+# secrets needs two values
+# apikey=... # see https://trello.com/1/appKey/generate#
+# token=...  # generate the private token URL via
+#            # http://pythonhosted.org/trello/examples.html
+#            # then visit in your web browser to approve
+#            # and paste in the resulting token
+
+# ---------------------------------------------------------------------
+# main
+# ---------------------------------------------------------------------
+
+
+def main():
+    "main"
+    trello = tr.TrelloApi(secrets.apikey, secrets.token)
+    columns = trello.boards.get_list(BOARD_ID)
+    cards = trello.boards.get_card(BOARD_ID)
+    owned = collections.defaultdict(list)
+    for card in cards:
+        columnId = card["idList"]
+        game = card["name"]
+        owned[columnId].append(game)
+    for column in columns:
+        columnName = column["name"]
+        columnId = column["id"]
+        print columnName
+        print "-" * len(columnName)
+        print "\n".join(owned[columnId])
+        print
+
+main()
