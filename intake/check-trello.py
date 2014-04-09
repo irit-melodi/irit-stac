4d87abc4fbd851aacce279ef07c4cf0bfa89b634
Eric Kow <eric.kow@gmail.com>
2014-04-09 15:29:57 +0000

    Report assigned members in corpus check script
    git-svn-id: https://wwwsecu.irit.fr/svn/Stac@2460 2e4a9ccd-9a2a-43a5-8ce1-c04977dfc52d

--- intake/check-trello.py
+++ intake/check-trello.py
@@ -27,15 +27,24 @@ BOARD_ID = '52453e3d9f6437111f001775'
 # ---------------------------------------------------------------------
 
 
+def card_info(card, names):
+    "string representing things we want to know about the card"
+    members = ", ".join(names[k] for k in card["idMembers"])
+    return card["name"] + (": " + members if members else "")
+
+
 def main():
     "main"
     trello = tr.TrelloApi(secrets.apikey, secrets.token)
     columns = trello.boards.get_list(BOARD_ID)
     cards = trello.boards.get_card(BOARD_ID)
     owned = collections.defaultdict(list)
+    names = {}
+    for member in trello.boards.get_member(BOARD_ID):
+        names[member["id"]] = member["fullName"]
     for card in cards:
         columnId = card["idList"]
-        game = card["name"]
+        game = card_info(card, names)
         owned[columnId].append(game)
     for column in columns:
         columnName = column["name"]
