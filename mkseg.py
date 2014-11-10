c4c251aee5499c958c7d2c268123a117aa44daf5
Eric Kow <eric.kow@gmail.com>
2014-11-10 17:18:44 +0000

    mkseg: more refactoring
    git-svn-id: https://wwwsecu.irit.fr/svn/Stac@3456 2e4a9ccd-9a2a-43a5-8ce1-c04977dfc52d

--- mkseg.py
+++ mkseg.py
@@ -4,13 +4,81 @@
 import sys
 from datetime import date
 from pprint import pprint
+from os import path as fp
 
 """
 This is an educified version of the code/UNUSED/guapi/PredArg.py
 script
 """
 
-def Create_Unit(Annotator,base_directory, uunits, uiter):
+RESOURCE_STATUSES =\
+    ["Givable", "Not givable",
+     "Receivable", "Not receivable",
+     "Possessed", "Not possessed"]
+
+def _resource_snippet(resource):
+    "output fragment for a resource"
+
+    status = resource.Status
+
+    if resource.Status == "?":
+        status = "Unknown"
+    elif resource.Status in RESOURCE_STATUSES:
+        status = resource.Status
+    else:
+        raise Exception("Unexpected resource status: ", resource.Status)
+    template = " {status} ({kind}, {quantity})"
+    return template.format(status=status,
+                           kind=resource.Kind,
+                           quantity=resource.Quantity)
+
+
+def _link_snippet(left, right):
+    "output fragment for an anaphoric link"
+    template = " #Anaphora Link:({left}, {right})"
+    return template.format(left=left.Text,
+                           right=right.Text)
+
+
+def _anaphor_snippet(left, right, anaphora):
+    "output fragment for anaphoric links"
+
+    result = ""
+    if left.Kind == "Anaphoric":
+        for anaphor in anaphora:
+            if anaphor.Left_argument == left.ID:
+                result += _link_snippet(anaphor.Full_Left_argument,
+                                        anaphor.Full_Right_argument)
+            if anaphor.Right_argument == right.ID:
+                result += _link_snippet(anaphor.Full_Right_argument,
+                                        anaphor.Full_Left_argument)
+    return result
+
+
+def _segments_to_lines(segments):
+    """
+    return lines of .seg output
+
+    insert a marker whenever turns are more than 1 apart
+    """
+    def turn_id(seg):
+        ":: Int"
+        return int(seg.split()[2])
+
+    sorted_segments = sorted((turn_id(s), s)
+                             for s in segments)
+
+    result = []
+    prev_turn = None
+    for turn, segment in sorted_segments:
+        if prev_turn is not None and turn - prev_turn > 1:
+            result.append("*******END TURN************")
+        result.append(segment)
+        prev_turn = turn
+    return result
+
+
+def Create_Unit(Annotator, base_directory, uunits, uiter):
     # find speakers and idTurn
 
 
@@ -18,7 +86,7 @@ def Create_Unit(Annotator,base_directory, uunits, uiter):
         speaker_idturn = dict()
         anaphors=[]
         for j in uunits:
-            if basename(j.Docfile)==docfile:
+            if fp.basename(j.Docfile)==docfile:
                 for i in j.Dialogues:
                     anaphors.append(i.Full_relations)
                     for k in i.Turns:
@@ -27,161 +95,71 @@ def Create_Unit(Annotator,base_directory, uunits, uiter):
 
         return speaker_idturn, anaphors
 
-    def get_ressource(seg,several_ressource_text,anaphors_detail,  result):
-         if len(seg[0].Resources) > 0:
-               for i in seg[0].Resources:
-                   if isinstance(i,Several_resources)==True:
-                       several_ressource_text.append(i.Text)
+    def get_ressource(seg, several_ressource_text, anaphors_detail, result):
+
+        seg0 = seg[0]
+        if len(seg0.Resources) > 0:
+            for i in seg0.Resources:
+                if isinstance(i, Several_resources):
+                    several_ressource_text.append(i.Text)
+
+        if len(seg0.Resources) > 0:
+            result = result + '#' +'   Resource: '
+            for j in seg0.Resources:
+                if several_ressource_text != []:
+                    if isinstance(j, Several_resources):
+                        resource = j.Resources[0]
+                        result += _resource_snippet(resource)
+                        result += _anaphor_snippet(resource, j, anaphors_detail)
+                    else:
+                        if j.Text.split()[0] not in several_ressource_text[0]:
+                            result += _resource_snippet(j)
+                            result += _anaphor_snippet(j, j, anaphors_detail)
+                #pas de schéma on traite les ressources normalement"
+                else:
+                    result += _resource_snippet(j)
+                    result += _anaphor_snippet(j, j, anaphors_detail)
+        return result
 
 
-         if len(seg[0].Resources) > 0:
-                    result = result + '#' +'   Resource: '
-                    for j in seg[0].Resources:
-
-                      if several_ressource_text!=[]:
-                          if isinstance(j,Several_resources)==True:
-                              #Je traite les scheméas a part avec les opérateurs
-                              if j.Resources[0].Status=="Givable":
-                                result = result + ' Givable '
-                              if j.Resources[0].Status=="Not givable":
-                                result = result + ' Not givable '
-                              if j.Resources[0].Status=="Receivable":
-                                result = result + ' Receivable '
-                              if j.Resources[0].Status=="Not receivable":
-                                result = result + ' Not receivable'
-                              if j.Resources[0].Status=="Possessed":
-                                result = result  + ' Possessed'
-                              if j.Resources[0].Status=="Not possessed":
-                                result = result  + ' Not Possessed '
-                              if j.Resources[0].Status=="?":
-                                result = result  + ' Unknown '
-
-                              result = result + '(' + j.Resources[0].Kind + ', ' + j.Resources[0].Quantity + ')'  + j.Operator +  '(' + j.Resources[1].Kind + ', ' + j.Resources[1].Quantity + ')'
-
-                              if j.Resources[0].Kind=="Anaphoric":
-                                 for anaph in anaphors_detail:
-                                     if anaph!=[]:
-                                         for a in anaph:
-                                             if a.Left_argument==j.Resources[0].ID:
-                                                 result=result+ ' #' + 'Anaphora Link:(' + str(a.Full_Left_argument.Text) + ' , ' + str(a.Full_Right_argument.Text) + ' )'
-                                             if a.Right_argument==j.ID:
-                                                 result=result+ ' #' + 'Anaphora Link:(' + str(a.Full_Right_argument.Text) + ' , ' + str(a.Full_Left_argument.Text) + ' )'
-
-                          else:
-                              if j.Text.split()[0] not in several_ressource_text[0]:
-
-                                if j.Status=="Givable":
-                                  result = result + ' Givable '
-                                if j.Status=="Not givable":
-                                  result = result + ' Not givable '
-                                if j.Status=="Receivable":
-                                  result = result + ' Receivable '
-                                if j.Status=="Not receivable":
-                                  result = result + ' Not receivable '
-                                if j.Status=="Possessed":
-                                  result = result  + ' Possessed '
-                                if j.Status==" Not possessed":
-                                  result = result  + ' Not Possessed '
-                                if j.Status=="?":
-                                  result = result  + ' Unknown '
-
-                                result = result + '(' + j.Kind
-                                result = result + ', ' + j.Quantity + ') '
-                                if j.Kind=="Anaphoric":
-                                 for anaph in anaphors_detail:
-                                     if anaph!=[]:
-                                         for a in anaph:
-                                             if a.Left_argument==j.ID:
-                                                 result=result+ ' #' + 'Anaphora Link:(' + str(a.Full_Left_argument.Text) + ' , ' + str(a.Full_Right_argument.Text) + ' )'
-                                             if a.Right_argument==j.ID:
-                                                 result=result+ ' #' + 'Anaphora Link:(' + str(a.Full_Right_argument.Text) + ' , ' + str(a.Full_Left_argument.Text) + ' )'
-
-                      #pas de schéma on traite les ressources normalement"
-                      else:
-                        if j.Status=="Givable":
-                              result = result + ' Givable '
-                        if j.Status=="Not givable":
-                              result = result + ' Not givable '
-                        if j.Status=="Receivable":
-                              result = result + ' Receivable '
-                        if j.Status=="Not receivable":
-                              result = result + ' Not receivable '
-                        if j.Status=="Possessed":
-                              result = result  + ' Possessed '
-                        if j.Status==" Not possessed":
-                              result = result  + ' Not Possessed '
-                        if j.Status=="?":
-                              result = result  + ' Unknown '
-                        result = result + '(' + j.Kind
-                        result = result + ', ' + j.Quantity + ') '
-
-                        if j.Kind=="Anaphoric":
-                                 for anaph in anaphors_detail:
-                                     if anaph!=[]:
-                                         for a in anaph:
-                                             if a.Left_argument==j.ID:
-                                                 result=result+ ' #' + 'Anaphora Link:(' + str(a.Full_Left_argument.Text) + ' , ' + str(a.Full_Right_argument.Text) + ' )'
-                                             if a.Right_argument==j.ID:
-                                                 result=result+ ' #' + 'Anaphora Link:(' + str(a.Full_Right_argument.Text) + ' , ' + str(a.Full_Left_argument.Text) + ' )'
-         return result
-
+    fbasename = fp.basename(seg[0].Textfile)
 
     segs = dict()
-
-    try:
-        for seg in uiter.get_segments():
-            result=''
-            if not basename(seg[0].Textfile) in segs:
-                segs[basename(seg[0].Textfile)] = []
-                speaker_idturn, anaphors=get_speaker_idturn(basename(seg[0].Textfile))
-                anaphors_detail=[]
-                for a in anaphors:
-                    if a!=[]:
-                       for i in a:
-                          anaphors_detail.append((i.ID,i.Left_argument,i.Right_argument))
-
+    for seg in uiter.get_segments():
+        result = ""
+        if fbasename not in segs:
+            segs[fbasename] = []
+            speaker_idturn, anaphors = get_speaker_idturn(fbasename)
+            anaphors_detail = []
+            for anaphor in anaphors:
+                for item in anaphor:
+                    anaphors_detail.append((item.ID,
+                                            item.Left_argument,
+                                            item.Right_argument))
 
 
-            speaker=speaker_idturn[seg[0].Turn]
-            result = result + str(seg[0]) + ' [' + 'Turn_ID: ' + str(seg[0].Turn) +  ' #' + '    EDU_Span: '+ str(seg[0].Text) + '#' +'   Speaker: '  + str(speaker) +  '#' +'  Surface_Act: ' + str(seg[0].Surface_act_type)
+        speaker = speaker_idturn[seg[0].Turn]
+        template = "{dialogue_act} [ Turn_ID: {turn_id} "+\
+                   "#    EDU_Span: {span}" +\
+                   "#   Speaker: {speaker}" +\
+                   "#  Surface_Act: {surface_act}"
+        result += template.format(dialogue_act=seg[0],
+                                  turn_id=seg[0].Turn,
+                                  edu_span=str(seg[0].Text),
+                                  speaker=str(speaker),
+                                  surface_act=str(seg[0].Surface_act_type))
 
+        for receiver in seg[0].Receivers:
+           result += "#   Addressee: {}".format(receiver)
 
-            for receiver in seg[0].Receivers:
-               result = result + '#' +'   Addressee: ' + receiver
+        #Traitement spécial pour les ressources multiples
+        #Recup la liste des ressources dans several ressources
+        several_ressource_text = []
+        # appends stuff to result
+        result = get_ressource(seg, several_ressource_text, anaphors, result)
+        result = result + ']' + '\n'
+        segs[fbasename].append(result)
 
-
-            #Traitement spécial pour les ressources multiples
-            #Recup la liste des ressources dans several ressources
-            several_ressource_text=[]
-            result=get_ressource(seg,several_ressource_text,anaphors, result)
-
-            result=result + ']' + '\n'
-
-            segs[basename(seg[0].Textfile)].append(result)
-
-
-    except TypeError:
-        pass
-
-
-
-    for (filename, segments) in segs.items() :
-        f = open(base_directory + filename + "_"+Annotator+ ".seg", "w")
-        segments_sorted=[]
-        #sort results by ID Turn
-        for seg in segments :
-
-            segments_sorted.append((seg.split()[2],seg))
-
-        '''print filename
-        print segments_sorted
-        x=raw_input("hkjhkj")'''
-        sorted_key=sorted(segments_sorted, key=lambda Turn_ID: int(Turn_ID[0]))
-        j=0
-        for seg in sorted_key:
-            if j>1:
-                if int(seg[0])-int(sorted_key[j-1][0])>1:
-                    f.write("*******END TURN************\n")
-            f.write(seg[1] + "\n")
-            j=j+1
-        f.close()
+    for filename, segments in segs.items():
+        with open(base_directory + filename + "_"+Annotator+ ".seg", "w") as f:
+            print "\n".join(_segments_to_lines(segments))
