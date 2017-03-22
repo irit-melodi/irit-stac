628e6fcfc5e855df74ab2b0e34343a6000b5fad2
moreymat <mathieu.morey@gmail.com>
2017-03-22 14:44:51 +0100

    ENH add Server and UI msgs at end of game: scores, +1VP, duration

--- segmentation/segmentation.py
+++ segmentation/segmentation.py
@@ -143,11 +143,14 @@ def fuse_segments(t,xs):
     # 2017-03-09 Game State message: resource count for each player
     resource_count = r'(.* has \d* resource(s)?\.)'
     # end Game State message
+    # gen5
     # 2017-03-21 time left Server message
     # ">>> Less than X minutes remaining. Type *ADDTIME* to
     # extend this game another Y minutes."
     time_left = r'(>>> Less than \d+ minutes remaining[\.])'
-    # end time left Server message
+    # 2017-03-22 final scores
+    final_scores = r'(.* has \d* points\.)'
+    # end gen5 messages
     interjections  = [ r'a+r*g*h+'
                      , 'bah'
                      , 'eww'
@@ -163,7 +166,8 @@ def fuse_segments(t,xs):
     fusible_left_re  = re.compile('|'.join(fusible_left), flags=re.IGNORECASE)
 
     # should be fused with its right neighbour
-    fusible_right = [resource_alloc, resource_count, interjection, time_left]
+    fusible_right = [resource_alloc, resource_count, interjection, time_left,
+                     final_scores]
     fusible_right_re = re.compile('|'.join(fusible_right), flags=re.IGNORECASE)
 
     if len(xs) < 2:
